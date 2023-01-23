3d785c484e12d29a1e1da2f6e312ed0f1a894811
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2023-01-23 18:48:41 -0500

    after tutor session

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,28 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories
+    // be sure to include its associated Prodcts
+    const allCategoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(allCategoryData);
+  }
+  catch (error) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const oneCategoryData = await Category.
+  }
+  catch (error) {
+    res.status
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -17,6 +33,7 @@ router.post('/', (req, res) => {
   // create a new category
 });
 
+// /(use a number from the seed data)
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
